36ac4d1028721411d17d1e540d3347a90f9f616a
ikathuria <ishani@kathuria.net>
2021-04-07 17:53:06 +0530

    image preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,20 +1,8 @@
 import numpy as np
 import cv2
 import os
-from image_processing import func
+from image_processing import preprocess
 
-# Create the directories
-if not os.path.exists("data2"):
-    os.makedirs("data2")
-
-if not os.path.exists("data2/train"):
-    os.makedirs("data2/train")
-
-if not os.path.exists("data2/test"):
-    os.makedirs("data2/test")
-
-path1 = "data1/"
-path2 = "data2/"
 
 labels = [
     "zero",
@@ -36,30 +24,31 @@ labels = [
     "ok",
 ]
 
-c1 = 0
-c2 = 0
+path1 = "data1/"
+path2 = "data2/"
 
-for directory in os.listdir(path1):
-    for label in os.listdir(path1 + directory):
-        if not os.path.exists(path2 + "train/" + label):
-            os.makedirs(path2 + "train/" + label)
-        if not os.path.exists(path2 + "test/" + label):
-            os.makedirs(path2 + "test/" + label)
+# Create the directories
+if not os.path.exists("data2"):
+    os.makedirs("data2")
 
-        for file in os.listdir(path1 + directory + "/" + label):
-            actual_path = path1 + directory + "/" + label + "/" + file
-            dest_path = path2 + directory + "/" + label + "/" + file
+for i in labels:
+    if not os.path.exists(path2 + i):
+        os.makedirs(path2 + i)
 
-            if directory == "train":
-                c1 += 1
-            else:
-                c2 += 1
 
-            img = cv2.imread(actual_path, 0)
-            bw_image = func(actual_path)
-            cv2.imwrite(dest_path, bw_image)
-        print(label, end=" ")
-    print("\n", directory, "FINISHED.")
+i = 0
+for label in os.listdir(path1):
+    for file in os.listdir(path1 + label):
+        i += 1
+        src_path = path1 + label + "/" + file
+        dest_path = path2 + label + "/" + file
 
-print("\n\nNumber of trianing images:", c1)
-print("Number of test images:", c2)
+        img = cv2.imread(src_path, 0)
+        bw_image = preprocess(src_path)
+
+        cv2.imwrite(dest_path, bw_image)
+
+    print(label, end=" ")
+print("\n", "FINISHED.")
+
+print("\n\nNumber of images:", i)
